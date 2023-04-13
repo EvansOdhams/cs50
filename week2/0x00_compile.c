4b69c23df04ed1871da1452c5070b0b1d98b64d0
EvansOdhams <evansodhams@gmail.com>
2023-04-13 09:07:36 +0300

    Introduction to CS50 Computer Science Week II Programme

---
+++ week2/0x00_compile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main - main goes here
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Hello World\n");
+
+	return (0);
+}
---
+++ week2/README.md
@@ -0,0 +1,3 @@
+Introduction to computer science weeek 2
+
+1. The steps involved in compilation of a C Programme
