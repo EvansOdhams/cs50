3b97c8b1fa8bf20de1fe15cbf302c842a22e3077
EvansOdhams <evansodhams@gmail.com>
2023-04-16 16:24:26 +0300

    Function that adds two integers

---
+++ week2/functions/0x01-add_two_ints.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+/**
+ * Main - main function goes here
+ *
+ * A function that adds two ints
+ *
+ * Return: sum;
+ */
+
+int add_two_ints(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
