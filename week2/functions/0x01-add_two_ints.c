896941ae24c980dd62acd98ad7e5f9cfe496fd7b
EvansOdhams <evansodhams@gmail.com>
2023-04-16 17:03:04 +0300

    A Function that Adds two integers

--- week2/functions/0x01-add_two_ints.c
+++ week2/functions/0x01-add_two_ints.c
@@ -1,17 +1,47 @@
 #include <stdio.h>
-#include <cs50.h>
 
 /**
  * Main - main function goes here
  *
- * A function that adds two ints
+ * A program that adds two integers together using a function.
  *
- * Return: sum;
+ * Return: 0 if successful.
  */
 
-int add_two_ints(int x, int y)
-{
-	int sum = x + y;
+// Function prototype
+int add_two_ints(int x, int y);
 
-	return (sum);
+int main(void)
+{
+        //Ask the user for input
+        printf("Give me an integer: ");
+        int x;
+        scanf("%i", &x);
+        printf("Give me another integer: ");
+        int y;
+        scanf("%i", &y);
+
+        //Add the two numbers together via a function call
+        int z = add_two_ints(x, y);
+
+        //output the result
+        printf("The sum of %i and %i is %i!", x, y, z);
 }
+
+/**
+ * add_two_ints - Function definition
+ *
+ * This function takes two integers as parameters and returns their sum.
+ *
+ * @x: First integer to add.
+ * @y: Second integer to add.
+ *
+ * Return: The sum of x and y.
+ */
+int add_two_ints(int a, int b)
+{
+        int sum = a + b;
+
+        return sum;
+}
+
